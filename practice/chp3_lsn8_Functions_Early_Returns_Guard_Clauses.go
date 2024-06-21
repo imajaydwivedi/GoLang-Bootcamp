0322d710b4306f1bb4c765ef01bef9fba3294d64
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-06-21 19:01:03 +0530

    updated scripts

---
+++ practice/chp3_lsn8_Functions_Early_Returns_Guard_Clauses.go
@@ -0,0 +1,45 @@
+import "errors"
+
+func divide(dividend, divisor int) (int, error) {
+	if divisor == 0 {
+		return 0, errors.New("Can't divide by zero")
+	}
+	return dividend / divisor, nil
+}
+
+func getInsuranceAmount(status insuranceStatus) int {
+	amount := 0
+	if !status.hasInsurance() {
+		amount = 1
+	} else {
+		if status.isTotaled() {
+			amount = 10000
+		} else {
+			if status.isDented() {
+				amount = 160
+				if status.isBigDent() {
+					amount = 270
+				}
+			} else {
+				amount = 0
+			}
+		}
+	}
+	return amount
+}
+
+func getInsuranceAmount_With_Gaurd_Clauses(status insuranceStatus) int {
+	if !status.hasInsurance() {
+		return 1
+	}
+	if status.isTotaled() {
+		return 10000
+	}
+	if !status.isDented() {
+		return 0
+	}
+	if status.isBigDent() {
+		return 270
+	}
+	return 160
+}
