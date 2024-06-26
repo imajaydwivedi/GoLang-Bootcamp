3e5aa4ae7cfbc640a58baf5623943b58615765b1
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-06-26 11:11:47 +0530

    intro to error handling

---
+++ freecodecamp/chp6_errors/lsn1_error_interface_intro.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
+}
+
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	costToCustomer, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0, err
+	}
+
+	costToSpouse, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0, err
+	}
+
+	return costToCustomer + costToSpouse, nil
+}
+
+func main() {
+	fmt.Println(sendSMSToCouple("Reminder for Anniversary!", "Happy Anniversary Wife!"))
+}
